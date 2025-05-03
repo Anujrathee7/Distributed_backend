1609e0796fd9f9790035fcf4f9d75cc1b658d7fc
avaneepk <119371288+avaneepk@users.noreply.github.com>
2025-05-04 01:15:54 +0300

    Database
    Implementation of the database server into the backend

--- appointment_service/main.py
+++ appointment_service/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from AzureConn import connect
 import json
 import threading
 import os
@@ -8,6 +9,7 @@ import os
 app = FastAPI()
 lock = threading.Lock()
 DATA_FILE = "appointment_service/appointments.json"
+conn = connect()
 
 
 origins = [
@@ -28,14 +30,34 @@ class Appointment(BaseModel):
     time: str
 
 def read_data():
-    if not os.path.exists(DATA_FILE):
+    '''if not os.path.exists(DATA_FILE):
         return []
     with open(DATA_FILE, 'r') as f:
-        return json.load(f)
+        return json.load(f)'''
+    # Connect to the PostgreSQL database and fetch appointments
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM appointments")
+    rows = cursor.fetchall()
+    appointments = []
+    for row in rows:
+        appointment = {
+            "patient": row[0],
+            "doctor": row[1],
+            "time": row[2]
+        }
+        appointments.append(appointment)
+    for appt in appointments:
+        print(appt)
+    
 
 def write_data(data):
-    with open(DATA_FILE, 'w') as f:
-        json.dump(data, f, indent=2)
+    '''with open(DATA_FILE, 'w') as f:
+        json.dump(data, f, indent=2)'''
+    cursor = conn.cursor()
+    for appt in data:
+        cursor.execute("INSERT INTO appointments (patient, doctor, time) VALUES (%s, %s, %s)", 
+                       (appt['patient'], appt['doctor'], appt['time']))
+
 
 @app.post("/book")
 def book(appt: Appointment):
